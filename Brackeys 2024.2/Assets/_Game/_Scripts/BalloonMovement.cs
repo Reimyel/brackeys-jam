3b79f89c96a892a3e8e4daba64df60921fbc79c0
Lucas Timar <101904786+lntimar@users.noreply.github.com>
2024-09-08 13:38:46 -0300

    Balão

---
+++ Brackeys 2024.2/Assets/_Game/_Scripts/BalloonMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMovement : MonoBehaviour
+{
+    [SerializeField] private float upSpeed;
+    [SerializeField] private float horizontalSpeed;
+
+    private Rigidbody2D _rb;
+
+    private float dirX;
+
+    void Start()
+    {
+       _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(0, upSpeed);
+
+        dirX = Input.GetAxisRaw("Horizontal");
+        _rb.velocity = new Vector2(dirX * horizontalSpeed, _rb.velocity.y);
+ 
+    }
+}
