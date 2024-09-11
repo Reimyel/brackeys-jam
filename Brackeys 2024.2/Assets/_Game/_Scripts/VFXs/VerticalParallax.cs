4debad368b0d8b9f3f45b1b549c9d1d5be339de7
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-09-10 23:32:15 -0300

    Parallax Vertical

---
+++ Brackeys 2024.2/Assets/_Game/_Scripts/VFXs/VerticalParallax.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class VerticalParallax : MonoBehaviour
+{
+    [Header("Configurações:")]
+    [SerializeField] private float verticalSpeed;
+
+    // Componentes:
+    private Rigidbody2D _rb;
+
+    private Vector3 _startPos;
+
+    private void Start() => _startPos = gameObject.transform.position;
+    
+    private void Update() => transform.position += verticalSpeed * Vector3.up * Time.deltaTime;
+
+    private void OnTriggerEnter2D(Collider2D collision) => gameObject.transform.position = _startPos;
+}
