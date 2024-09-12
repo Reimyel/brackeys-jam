8999f49b2ad4c7382476fd0f7928c18595a0f43e
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-09-11 23:42:11 -0300

    Ligando Status com Scripts do Balão

--- Brackeys 2024.2/Assets/_Game/_Scripts/Entities/Balloon/GunBehaviourScript.cs
+++ Brackeys 2024.2/Assets/_Game/_Scripts/Entities/Balloon/GunBehaviourScript.cs
@@ -10,7 +10,7 @@ public class GunBehaviourScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Cow"))
+        if (other.CompareTag("Cow") && BalloonStats.HasGun)
         {
             GameObject projectile = Instantiate(projectilePrefab, projectileSpawnpoint.position, Quaternion.identity);
 
