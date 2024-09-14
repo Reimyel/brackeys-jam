52b4651af0b0213a5c422bacd0ea16f62e585432
Reimyel <caiquegsambrana@gmail.com>
2024-09-14 13:27:56 -0300

    mony

--- Brackeys 2024.2/Assets/_Game/_Scripts/Managers/MoneySpawnManager.cs
+++ Brackeys 2024.2/Assets/_Game/_Scripts/Managers/MoneySpawnManager.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class MoneySpawnManager : MonoBehaviour
 {
     #region Referências
+    [SerializeField] private float minTime;
+    [SerializeField] private float maxTime;
     [SerializeField] private GameObject moneyObject;
     [SerializeField] private Transform[] moneySpawnPoints;
     #endregion
 
     #region Funções Unity
+    private void Update()
+    {
+        StartCoroutine(SpawnMoney(minTime, maxTime));
+    }
     #endregion
 
     #region Funções Próprias
+    private IEnumerator SpawnMoney(float minTime, float maxTime)
+    {
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+
+        var instance = Instantiate(moneyObject, moneySpawnPoints[Random.Range(0, moneySpawnPoints.Length)].position, moneyObject.transform.rotation);
+
+        ObstacleBehaviourScript behaviourScript = instance.GetComponent<ObstacleBehaviourScript>();
+        if (behaviourScript != null)
+        {
+            behaviourScript.SetDirection(Vector2.left);
+        }
+
+        StartCoroutine(SpawnMoney(minTime, maxTime));
+    }
     #endregion
 }
