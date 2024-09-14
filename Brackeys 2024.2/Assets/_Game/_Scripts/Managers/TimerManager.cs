b15d21135c4fff2b1e05fe503dd8b4dc017a5650
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-09-14 14:15:39 -0300

    Lógica Fim do Jogo

--- Brackeys 2024.2/Assets/_Game/_Scripts/Managers/TimerManager.cs
+++ Brackeys 2024.2/Assets/_Game/_Scripts/Managers/TimerManager.cs
@@ -1,14 +1,34 @@
+using EasyTransition;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerManager : MonoBehaviour
 {
     #region Variáveis
-    [SerializeField] private TextMeshProUGUI timerText;
+    [Header("Configurações:")]
     [SerializeField] private float timeToStage2;
     [SerializeField] private float timeToStage3;
+    [SerializeField] private float timeToEnd;
+    [SerializeField] private Texture2D spriteStage2;
+    [SerializeField] private Texture2D spriteStage3;
+    [SerializeField] private float endForce;
+
+    [Header("Transição:")]
+    [SerializeField] private string endSceneName;
+    [SerializeField] private TransitionSettings transitionSettings;
+    [SerializeField] private float loadTime;
+
+    [Header("Referências:")]
+    [SerializeField] private Rigidbody2D rbBaloon;
+    [SerializeField] private PolygonCollider2D polygonColliderBalloon;
+    [SerializeField] private EdgeCollider2D edgeColliderBalloon;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private RawImage imgParallax;
+
+
     public float time;
     public float timeCounter;
     private bool stage2Triggered = false;
@@ -43,12 +63,25 @@ public class TimerManager : MonoBehaviour
     {
         Debug.Log("FASE 2");
         //lógica de transição de fases
+        imgParallax.texture = spriteStage2;
     }
 
     void Stage3Begin()
     {
         Debug.Log("FASE 3");
         //lógica de transição de fases
+        imgParallax.texture = spriteStage3;
+    }
+
+    void EndGame() 
+    {
+        Debug.Log("FIM");
+        // Fazer o balão subir
+        polygonColliderBalloon.enabled = false;
+        edgeColliderBalloon.enabled = false;
+
+        rbBaloon.AddForce(Vector2.up * endForce * Time.deltaTime);
+        TransitionManager.Instance().Transition(endSceneName, transitionSettings, loadTime);
     }
     #endregion
 }
