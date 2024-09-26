426567e90a303a7b412d9291d65aa36be14bbfc5
Reimyel <caiquegsambrana@gmail.com>
2024-09-26 12:03:50 -0300

    quase escopeta

---
+++ Brackeys 2024.2/Assets/_Game/_Scripts/CursorVisibility.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisibility : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Cursor.visible = false;
+    }
+}
--- Brackeys 2024.2/Assets/_Game/_Scripts/TESTES/GunScript.cs
+++ Brackeys 2024.2/Assets/_Game/_Scripts/TESTES/GunScript.cs
@@ -7,21 +7,25 @@ public class GunScript : MonoBehaviour
 {
     #region Variáveis
     [Header("Stats:")]
-    //[SerializeField] private int projectileCount = 3;
     [SerializeField] private float projectileSpeed = 60f;
-    //[SerializeField] private Vector3 offset = new Vector3(0f, 0.5f, 0f);
+    [SerializeField] float fireRate = 1f;
+    [SerializeField] private int projectileCount = 5;
+    [SerializeField] private float spreadAngle = 15f;
 
     [Header("GFX:")]
     [SerializeField] private SpriteRenderer playerHeadSpr;
+    [SerializeField] private Sprite idleSprite;
     [SerializeField] private Sprite shootSprite;
-    //[SerializeField] private Transform playerHeadTransform;
     [SerializeField] private GameObject crosshair;
 
     [Header("Referências:")]
     [SerializeField] private GameObject headHolder;
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform projectileSpawnpoint;
+
     private Vector3 _target;
+    private float upTreshold = 10f;
+    private bool canShoot = true;
     #endregion
 
     private void Start()
@@ -34,28 +38,48 @@ public class GunScript : MonoBehaviour
 
     private void Update()
     {
-        Cursor.visible = false;
-
         _target = FindObjectOfType<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(FindObjectOfType<Camera>().transform.position.z)));
         crosshair.transform.position = new Vector2(_target.x, _target.y);
 
         Vector3 difference = _target - headHolder.transform.position;
-        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        headHolder.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
-        if (Input.GetMouseButtonDown(0))
+        if (_target.y > headHolder.transform.position.y + upTreshold)
+        {
+            //mouse encima
+            headHolder.transform.localScale = new Vector3(1, 1, 1);
+            headHolder.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+        else
+        {
+            headHolder.transform.rotation = Quaternion.identity;
+
+            if (_target.x > headHolder.transform.position.x)
+            {
+                //mouse na direita
+                headHolder.transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (_target.x < headHolder.transform.position.x)
+            {
+                //mouse na esquerda
+                headHolder.transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && canShoot)
         {
             float distance = difference.magnitude;
-            Vector2 direction = difference / distance;
+            Vector2 direction = difference.normalized;
             direction.Normalize();
-            Shoot(direction, rotationZ);
+            Shoot(direction);
 
             Debug.Log("BALA NA AGULHA");
             if (AudioManager.Instance != null)
                 AudioManager.Instance.PlaySFX("Shoot");
 
+            StartCoroutine(ChangeSprite());
             Invoke("ReloadSFX", 0.5f);
-            //Invoke("ResetSprite", 1f);
+
+            StartCoroutine(ShootCooldown());
         }
 
         /*if (!BalloonStats.HasGun)
@@ -78,12 +102,33 @@ public class GunScript : MonoBehaviour
         }*/
     }
 
-    void Shoot(Vector2 direction, float rotationZ)
+    void Shoot(Vector2 direction)
     {
-        GameObject _projectile = Instantiate(projectilePrefab) as GameObject;
-        _projectile.transform.position = projectileSpawnpoint.transform.position;
-        _projectile.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-        _projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+        for (int i = 0; i < projectileCount; i++)
+        {
+            float angle = Random.Range(-spreadAngle, spreadAngle); //dispersão aleatória definida
+            Vector2 spreadDirection = Quaternion.Euler(0, 0, angle) * direction;
+
+            GameObject _projectile = Instantiate(projectilePrefab) as GameObject;
+            _projectile.transform.position = projectileSpawnpoint.transform.position;
+            _projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+        }
+    }
+
+    IEnumerator ChangeSprite()
+    {
+        playerHeadSpr.sprite = shootSprite;
+
+        yield return new WaitForSeconds(1f);
+
+        playerHeadSpr.sprite = idleSprite;
+    }
+
+    IEnumerator ShootCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
     }
 
     private void ReloadSFX()
