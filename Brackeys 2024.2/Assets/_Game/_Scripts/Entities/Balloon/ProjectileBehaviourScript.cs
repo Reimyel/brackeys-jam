5dc968b185011b52135da208ad239f4674a13af1
Reimyel <caiquegsambrana@gmail.com>
2024-09-27 15:29:22 -0300

    arrumando prefab

--- Brackeys 2024.2/Assets/_Game/_Scripts/Entities/Balloon/ProjectileBehaviourScript.cs
+++ Brackeys 2024.2/Assets/_Game/_Scripts/Entities/Balloon/ProjectileBehaviourScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ProjectileBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private Color fadeColor;
+    [SerializeField] private FadeVFX fadePrefab;
+
+    private void Start()
+    {
+        StartCoroutine(ApplyEffect(0.01f));
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == 11)
@@ -22,5 +30,22 @@ public class ProjectileBehaviourScript : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         Destroy(gameObject);
+    }
+
+    private IEnumerator ApplyEffect(float t)
+    {
+        yield return new WaitForSeconds(t);
+        var effect = Instantiate(fadePrefab, transform.position, Quaternion.identity);
+        var effectSpr = effect.GetComponent<SpriteRenderer>();
+
+        // Configurando Sprite do Fade
+        effectSpr.sprite = GetComponent<SpriteRenderer>().sprite;
+        effectSpr.color = fadeColor;
+        effectSpr.gameObject.transform.localScale = gameObject.transform.localScale;
+
+        // Colocando a referência para rotação
+        effect.GetComponent<FadeVFX>().RotationParent = gameObject.transform;
+
+        StartCoroutine(ApplyEffect(0.01f));
     }
 }
